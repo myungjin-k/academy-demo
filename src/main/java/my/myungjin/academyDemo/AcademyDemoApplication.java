52ecbe884356542bc74314fe44b574a525f2f085
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2020-11-20 17:25:25 +0900

    Initial commit

---
+++ src/main/java/my/myungjin/academyDemo/AcademyDemoApplication.java
@@ -0,0 +1,13 @@
+package my.myungjin.academyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyDemoApplication.class, args);
+	}
+
+}
