430c7b444ded94439823adcf4373530686f0de88
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2020-11-21 03:18:02 +0900

    코드그룹 도메인 작성

---
+++ src/main/java/my/myungjin/academyDemo/domain/common/CodeGroup.java
@@ -0,0 +1,62 @@
+package my.myungjin.academyDemo.domain.common;
+
+import lombok.*;
+import my.myungjin.academyDemo.util.Util;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.time.LocalDateTime.now;
+import static my.myungjin.academyDemo.util.Util.timestampOf;
+
+@Entity
+@Table(name = "code_group")
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"})
+public class CodeGroup {
+    @Id
+    private String id;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "name_eng", nullable = false)
+    private String nameEng;
+
+    @Column(name = "name_kor", nullable = false)
+    private String nameKor;
+
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private Timestamp createAt;
+
+    @Column(name = "update_at", nullable = false)
+    private Timestamp updateAt;
+
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "group_id")
+    private List<CommonCode> codes = new ArrayList<>();
+
+    @Builder
+    public CodeGroup(String code, String nameEng, String nameKor) {
+        this.id = Util.getUUID();
+        this.code = code;
+        this.nameEng = nameEng;
+        this.nameKor = nameKor;
+        Timestamp now = timestampOf(now());
+        this.createAt = now;
+        this.updateAt = now;
+    }
+
+    public void update(String code, String nameEng, String nameKor){
+        this.code = code;
+        this.nameEng = nameEng;
+        this.nameKor= nameKor;
+        this.updateAt = timestampOf(now());
+    }
+
+}
---
+++ src/main/java/my/myungjin/academyDemo/domain/common/CodeGroupRepository.java
@@ -0,0 +1,20 @@
+package my.myungjin.academyDemo.domain.common;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CodeGroupRepository extends JpaRepository<CodeGroup, String> {
+
+    @Query("SELECT cg FROM CodeGroup cg ORDER BY cg.createAt")
+    List<CodeGroup> findAll();
+
+    Optional<CodeGroup> findByCode(String code);
+
+    @Query("UPDATE CodeGroup cg SET cg.code = :code, cg.nameEng = :nameEng, cg.nameKor = :nameKor WHERE cg.id = :id")
+    void update(@Param("id") String id, @Param("code") String code, @Param("nameEng") String nameEng, @Param("nameKor") String nameKor);
+
+}
