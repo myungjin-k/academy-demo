202e06ada6ad54422aeab3be4cb781c180174f4a
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2020-12-15 19:47:56 +0900

    swagger ui try it out 테스트

--- src/main/java/my/myungjin/academyDemo/web/controller/ItemSaleController.java
+++ src/main/java/my/myungjin/academyDemo/web/controller/ItemSaleController.java
@@ -1,24 +1,28 @@
 package my.myungjin.academyDemo.web.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import my.myungjin.academyDemo.domain.item.ItemMaster;
 import my.myungjin.academyDemo.service.item.ItemMasterService;
 import my.myungjin.academyDemo.web.Response;
 import my.myungjin.academyDemo.web.request.PageRequest;
 import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static my.myungjin.academyDemo.web.Response.OK;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/mall/item")
-@Controller
+@RestController
 public class ItemSaleController {
     private final ItemMasterService itemMasterService;
 
-    @GetMapping("/all")
+    @GetMapping(path = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "전시상품 전체 리스트 조회(API 키 필요없음)")
     public Response<Page<ItemMaster>> allItems(PageRequest pageRequest){
         return OK(
                 itemMasterService.findAllItems(pageRequest.of())
