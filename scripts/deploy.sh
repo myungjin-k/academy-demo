b0410184ac7000f07b8e9c2235300ae96cd266fa
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2021-01-07 22:42:57 +0900

    JAR NAME 설정 및 권한추가 오류 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,12 +23,12 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +s $JAR_NAME
+chmod +x $JAR_NAME
 
 nohup java -jar -Dspring.config.location=classpath:/application-real.yml,classpath:/application-real-admin.yml,/home/ec2-user/app/application-mail.yml,/home/ec2-user/app/application-cloud.yml,/home/ec2-user/app/application-real-db.yml -Dspring.profiles.active=real,real-admin  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
