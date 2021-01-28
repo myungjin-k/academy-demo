600f871590087364e15927ac43ee83c2837bb92d
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2021-01-28 20:09:54 +0900

    주문상세 프론트 작업중   -- 상품 정가 가져와야

--- src/main/resources/templates/static/js/myOrder.js
+++ src/main/resources/templates/static/js/myOrder.js
@@ -31,7 +31,7 @@ var myOrder = {
     }
 };
 var myOrderDetail = {
-    div : $('#div-my-order-list'),
+    div : $('#div-my-order-detail'),
     userId : '',
     orderId : '',
     init : function(userId, orderId) {
@@ -42,6 +42,28 @@ var myOrderDetail = {
         $('.contentDiv').not(_this.div).addClass('d-none');
         _this.div.removeClass('d-none');
     },
+    makeItemRow : function(item){
+      let row = $('<tr/>');
+
+      const itemLink = $('<a href="" onclick="return false;"/>')
+            .append($('<input type="hidden"/>')
+                .prop('name', 'itemDisplayId')
+                .val(item.itemDisplayId))
+            .append($('<img class="thumbnail mr-3"/>')
+                .prop('src', item.thumbnail))
+            .append(item.itemName)
+            .append($('<br/>'))
+            .append('\n' + item.color + '/' + item.size);
+
+        //const salePrice = $('<div class="salePrice"/>').text(display.salePrice * cartItem.count);
+
+        row
+            .append($('<td class="itemInfo"/>').append(itemLink))
+            .append($('<td class="itemCount"/>').append(item.count))
+            .append($('<td class="itemPrice"/>').append(item.itemPrice))
+            .append($('<td class="deliveryStatus"/>').append(item.deliveryStatus + '\n' + item.invoiceNum));
+        return row;
+    },
     load : function (){
         var _this = this;
         $.ajax({
@@ -51,6 +73,24 @@ var myOrderDetail = {
             contentType:'application/json; charset=utf-8'
         }).done(function(response) {
             console.log(response);
+            const order = response.response;
+            _this.div.find('input[name="orderId"]').val(order.orderId);
+
+            // TODO 정가 있어야함
+            const items = order.items;
+            const itemDiv = _this.div.find('#order-items');
+            itemDiv.empty();
+            $(items).each(function(){
+                const row = _this.makeItemRow(this);
+                itemDiv.append(row);
+            });
+            const orderDiv = _this.div.find('.orderInfo');
+            orderDiv.find('#orderName').text(order.orderName);
+            orderDiv.find('#orderTel').text(order.orderTel);
+            const deliverDiv = _this.div.find('.deliverInfo');
+            deliverDiv.find('#receiverName').text(order.deliveryName);
+            deliverDiv.find('#receiverTel').text(order.deliveryTel);
+            deliverDiv.find('#receiverAddr').text(order.deliveryAddr);
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
