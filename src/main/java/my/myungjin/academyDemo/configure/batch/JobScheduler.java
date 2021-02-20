53144dc7ec4d36de40e65a699b3162cb4ec6c99a
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2021-02-20 18:59:15 +0900

    Job Scheduler 등록

---
+++ src/main/java/my/myungjin/academyDemo/configure/batch/JobScheduler.java
@@ -0,0 +1,43 @@
+package my.myungjin.academyDemo.configure.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JobScheduler {
+
+    private final JobLauncher jobLauncher;
+
+    private final DeliveryStatusJobConfigure deliveryStatusJobConfigure;
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 3000)
+    public void runJob() {
+
+        Map<String, JobParameter> confMap = new HashMap<>();
+        confMap.put("time", new JobParameter(System.currentTimeMillis()));
+        JobParameters jobParameters = new JobParameters(confMap);
+        try {
+
+            jobLauncher.run(deliveryStatusJobConfigure.deliveryStatusJob(), jobParameters);
+
+        } catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
+                | JobParametersInvalidException | org.springframework.batch.core.repository.JobRestartException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+
+}
