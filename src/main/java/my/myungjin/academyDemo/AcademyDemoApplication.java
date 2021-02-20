1d5dc307b55a45854ec770cf44946d0df05fa306
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2021-02-20 19:34:11 +0900

    Job Scheduler 등록

--- src/main/java/my/myungjin/academyDemo/AcademyDemoApplication.java
+++ src/main/java/my/myungjin/academyDemo/AcademyDemoApplication.java
@@ -4,7 +4,9 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableBatchProcessing
 @EnableCaching
 @SpringBootApplication
