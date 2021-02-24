09b6f3867d6c9dcb68d2fd448931a4524d111ac2
myungjin-k <63644217+myungjin-k@users.noreply.github.com>
2021-02-24 17:39:12 +0900

    배포 시 payment profile 누락으로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,4 +31,4 @@ echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-nohup java -jar -Dspring.config.location=classpath:/application-real.yml,/home/ec2-user/app/application-mail.yml,/home/ec2-user/app/application-cloud.yml,/home/ec2-user/app/application-real-db.yml -Dspring.profiles.active=real  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application-real.yml,/home/ec2-user/app/application-mail.yml,/home/ec2-user/app/application-cloud.yml,/home/ec2-user/app/application-real-db.yml,/home/ec2-user/app/application-payment.yml -Dspring.profiles.active=real  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
